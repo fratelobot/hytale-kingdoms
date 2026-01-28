bd521188d93a7ba5b4896fdba84d4d53874e2869
Fratelo <fratelo@agentmail.to>
2026-01-28 21:19:24 +0000

    feat: Use AbstractPlayerCommand for proper player position access
    Commands now extend AbstractPlayerCommand which provides:
    - Store<EntityStore> for component access
    - Ref<EntityStore> for entity reference
    - PlayerRef for player-specific data
    - World context
    
    Position obtained via TransformComponent:
      TransformComponent transform = store.getComponent(ref, TransformComponent.getComponentType());
      Vector3d position = transform.getPosition();
    
    Based on Hytale's WhereAmICommand pattern from:
    https://github.com/Ranork/Hytale-Server-Unpacked

--- src/main/java/com/kingdoms/plugin/commands/BuildLumbermillCommand.java
+++ src/main/java/com/kingdoms/plugin/commands/BuildLumbermillCommand.java
@@ -1,34 +1,49 @@
 package com.kingdoms.plugin.commands;
 
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.protocol.GameMode;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
-import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.kingdoms.plugin.KingdomsPlugin;
 import com.kingdoms.plugin.building.BuildingType;
 
 import javax.annotation.Nonnull;
 
 /**
- * Command to build a Lumbermill
+ * Command to build a Lumbermill at the player's position
  * Usage: /build_lumbermill
  */
-public class BuildLumbermillCommand extends CommandBase {
+public class BuildLumbermillCommand extends AbstractPlayerCommand {
 
     private final KingdomsPlugin plugin;
 
     public BuildLumbermillCommand(KingdomsPlugin plugin) {
-        super("build_lumbermill", "Start construction of a Lumbermill");
+        super("build_lumbermill", "server.commands.kingdoms.build_lumbermill.desc");
         this.setPermissionGroup(GameMode.Adventure);
         this.plugin = plugin;
     }
 
     @Override
-    protected void executeSync(@Nonnull CommandContext ctx) {
-        // Fixed position offset from Town Hall
-        int x = 10;
-        int y = 64;
-        int z = 0;
+    protected void execute(@Nonnull CommandContext context, 
+                          @Nonnull Store<EntityStore> store, 
+                          @Nonnull Ref<EntityStore> ref, 
+                          @Nonnull PlayerRef playerRef, 
+                          @Nonnull World world) {
         
-        plugin.getBuildingManager().startConstruction(ctx, BuildingType.LUMBERMILL, x, y, z);
+        TransformComponent transform = (TransformComponent) store.getComponent(ref, TransformComponent.getComponentType());
+        assert transform != null;
+        
+        Vector3d position = transform.getPosition();
+        int x = (int) position.getX();
+        int y = (int) position.getY();
+        int z = (int) position.getZ();
+        
+        plugin.getBuildingManager().startConstruction(context, BuildingType.LUMBERMILL, x, y, z);
     }
 }
--- src/main/java/com/kingdoms/plugin/commands/BuildTownHallCommand.java
+++ src/main/java/com/kingdoms/plugin/commands/BuildTownHallCommand.java
@@ -1,38 +1,50 @@
 package com.kingdoms.plugin.commands;
 
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.protocol.GameMode;
-import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
-import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.kingdoms.plugin.KingdomsPlugin;
 import com.kingdoms.plugin.building.BuildingType;
 
 import javax.annotation.Nonnull;
 
 /**
- * Command to build a Town Hall
+ * Command to build a Town Hall at the player's position
  * Usage: /build_townhall
- * 
- * TODO: Get actual player position when API is available
  */
-public class BuildTownHallCommand extends CommandBase {
+public class BuildTownHallCommand extends AbstractPlayerCommand {
 
     private final KingdomsPlugin plugin;
 
     public BuildTownHallCommand(KingdomsPlugin plugin) {
-        super("build_townhall", "Start construction of a Town Hall");
+        super("build_townhall", "server.commands.kingdoms.build_townhall.desc");
         this.setPermissionGroup(GameMode.Adventure);
         this.plugin = plugin;
     }
 
     @Override
-    protected void executeSync(@Nonnull CommandContext ctx) {
-        // TODO: Get player position from context when API is available
-        // For now, use fixed position
-        int x = 0;
-        int y = 64;
-        int z = 0;
+    protected void execute(@Nonnull CommandContext context, 
+                          @Nonnull Store<EntityStore> store, 
+                          @Nonnull Ref<EntityStore> ref, 
+                          @Nonnull PlayerRef playerRef, 
+                          @Nonnull World world) {
         
-        plugin.getBuildingManager().startConstruction(ctx, BuildingType.TOWN_HALL, x, y, z);
+        // Get player position from TransformComponent
+        TransformComponent transform = (TransformComponent) store.getComponent(ref, TransformComponent.getComponentType());
+        assert transform != null;
+        
+        Vector3d position = transform.getPosition();
+        int x = (int) position.getX();
+        int y = (int) position.getY();
+        int z = (int) position.getZ();
+        
+        plugin.getBuildingManager().startConstruction(context, BuildingType.TOWN_HALL, x, y, z);
     }
 }
